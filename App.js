29cc2f7839c1974c5974135b0b1a3ff033411346
fourestfire <kewliez@gmail.com>
2017-04-18 00:30:20 -0400

    wrote flexbox example

---
+++ App.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button
+} from 'react-native';
+
+import Dimensions from 'Dimensions';
+let {height, width} = Dimensions.get('window');
+console.log(width)
+
+const sampleContact = {
+  Name: 'Damon Doggo',
+  Address: '123 Main Street',
+  PhoneNumber: '212-787-4949',
+  FavoriteFood: 'Pizza',
+  Hobbies: 'Tennis, Frisbee'
+}
+
+export default class flexbox extends Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.profilePicture}>
+          <Image source={require('./doggo.jpg')} style={{height: 140, width: 140, borderRadius: 70, marginBottom: 20}}/>
+          <Text style={[styles.text, {fontSize: 27, fontWeight:'100'}]}> {sampleContact.Name.split(' ')[0]} </Text>
+        </View>
+
+        {/*<View style={styles.divider} />*/}
+        {
+          Object.keys(sampleContact).map(key => {
+            return (
+              <View key={key} style={styles.contactRow}>
+                <Text style={styles.contactKey}> {key.replace(/([A-Z])/g, ' $1')} </Text>
+                <Text style={styles.contactValue}> {sampleContact[key]} </Text>
+              </View>
+            );
+          })
+        }
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 30,
+  },
+
+  divider: {
+    borderBottomWidth: 1,
+    borderColor: 'lightgrey',
+  },
+
+  text: {
+    fontSize: 16,
+  },
+
+  profilePicture: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'ghostwhite',
+    flex: 1,
+    borderRadius: 10
+  },
+
+  contactRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'lightgrey',
+  },
+
+  contactKey: {
+    marginLeft: 10,
+    width: 120,
+    fontWeight: 'bold',
+  },
+
+  contactValue: {
+    paddingVertical: 28
+  }
+
+});
+
+AppRegistry.registerComponent('flexbox', () => flexbox);
+
